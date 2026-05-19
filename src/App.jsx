7bbd8b89412f66f2642423f3358358e353868d79
Esbjorn188 <esser181800@gmail.com>
2026-05-19 11:38:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css"
+import Home from "./components/Home"
+import About from "./components/About"
+import News from "./components/News"
+import Contact from "./components/Contact"
+
+function App() {
+
+return (
+  <div><h1 className="text-5xl text-red-500">Her er en text</h1></div>
+  
+)
+
+
+
+}
+
+export default App
